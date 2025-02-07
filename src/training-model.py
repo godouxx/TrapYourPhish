d77002af0b5d1fc7fe73b025eacf798140f53314
Oxbian <oxbian@mailbox.org>
2025-02-07 18:10:15 -0500

    feat: comparaison des classifieurs et des vectorizeur

--- src/training-model.py
+++ src/training-model.py
@@ -1,124 +1,184 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.preprocessing import LabelEncoder
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split  # , cross_val_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, ConfusionMatrixDisplay, roc_auc_score, roc_curve
 import joblib as jb
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (ConfusionMatrixDisplay, accuracy_score, f1_score,
+                             precision_score, recall_score, roc_auc_score,
+                             roc_curve)
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.svm import SVC
 
 # Import du dataset
-phishing_data = pd.read_csv("Data/Phishing_Email.csv")
-
-# Copie du dataset
-phishing_df = phishing_data.rename(columns={'Unnamed: 0': 'ID'})
+phishing_df = pd.read_csv("datasets/mail-phishing.csv")
 
 # Récupération des informations sur le dataset
 print(phishing_df.head())
-phishing_df.info()
+print(phishing_df.info())
 
 # Compter le type de mail
-print(f"\n\nType de mail: {phishing_df["Email Type"].value_counts()}\n")
+print(f"\n\nType de mail: {phishing_df["isPhishing"].value_counts()}\n")
 
 # Tri / suppression des données nulles
 print(f"Données nulles:\n{phishing_df.isna().sum()}\n")
 phishing_df = phishing_df.dropna()
 print(f"Données nulles:\n{phishing_df.isna().sum()}\n")
-phishing_df = phishing_df[~phishing_df['Email Text'].str.contains('empty')]
 
 # Vérification des duplicats
 duplicates = phishing_df[phishing_df.duplicated(keep=False)]
-print(duplicates)
+print(f"Données dupliquées: \n{duplicates}")
 
 # Comptage après le clean
-print(f"\n\nType de mail: {phishing_df["Email Type"].value_counts()}\n")
+print(f"\n\nType de mail: {phishing_df["isPhishing"].value_counts()}\n")
 
 # Affichage sous forme de graphe
-plt.figure(figsize=(7, 5))
-ax = sns.countplot(x="Email Type", hue="Email Type",
-                   data=phishing_df, palette="viridis")
+is_phishing_counts = phishing_df['isPhishing'].value_counts()
 
-for i in range(0, len(ax.patches)):
-    ax.bar_label(ax.containers[i])
+plt.figure(figsize=(6, 4))
+ax = is_phishing_counts.plot(kind='bar', color=['#ff6347', '#90ee90'])
 
-plt.title("Distribution des types d'emails")
-plt.xlabel("Type d'email")
-plt.ylabel("Nombre")
-plt.xticks(rotation=45)
+# Ajouter le nombre au-dessus de chaque barre
+for p in ax.patches:
+    ax.annotate(f'{p.get_height()}',
+                (p.get_x() + p.get_width() / 2., p.get_height()),
+                ha='center', va='center',
+                fontsize=12, color='black',
+                xytext=(0, 5), textcoords='offset points')
+
+# Personnalisation du graphique
+plt.title('Répartition des Mails (Phishing vs Normal)', fontsize=14)
+plt.xlabel('Type de Mail', fontsize=12)
+plt.ylabel('Nombre de Mails', fontsize=12)
+plt.xticks([0, 1], ['Mails Phishing (1)', 'Mails Normaux (0)'], rotation=0)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+# Affichage du graphique
 plt.tight_layout()
 plt.show()
 
 # Équilibrer les classes avec de l'undersampling
-safe_mail = phishing_df[phishing_df["Email Type"] == "Safe Email"]
-phish_mail = phishing_df[phishing_df["Email Type"] == "Phishing Email"]
-safe_mail = safe_mail.sample(phish_mail.shape[0])
+safe_mail = phishing_df[phishing_df["isPhishing"] == 0]
+phish_mail = phishing_df[phishing_df["isPhishing"] == 1]
+phish_mail = phish_mail.sample(safe_mail.shape[0])
 
 print(f"Vérification du nombre de lignes: {
       safe_mail.shape, phish_mail.shape}\n")
 
 # Assembler les deux sous datasets
-phish_eq_df = pd.concat(
+phish_df = pd.concat(
     [safe_mail, phish_mail], ignore_index=True)
-phish_eq_df = phish_eq_df.sample(
+phish_df = phish_df.sample(
     frac=1, random_state=42).reset_index(drop=True)
-print(f"Dataset équilibrés: {phish_eq_df.head()}")
+print(f"Dataset équilibrés: {phish_df.head()}")
 
-# Encodage des types de mail
-label_encoder = LabelEncoder()
-phish_eq_df['Email Type'] = label_encoder.fit_transform(
-    phish_eq_df['Email Type'])
-print(f"Dataset encodée: {phish_eq_df}\n")
-phish_eq_df.drop(columns=['ID'], inplace=True)
-jb.dump(label_encoder, "models/encoder.pkl")
 
-# Vectorisation des textes des email
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(phish_eq_df["Email Text"])  # Input du modèle
-y = phish_eq_df['Email Type']  # Output du modèle (à prédire)
-jb.dump(vectorizer, "models/text-vectoriser.pkl")
+# Création d'un texte combiner des colonnes
+phish_df["text"] = "Date: " + phish_df["date"] + "\nFrom: " + phish_df["sender"] + \
+    "\nSubject: " + phish_df["subject"] + "\nTo: " + \
+    phish_df["receiver"] + "\n\n" + phish_df["body"]
+phishing_df.drop(['sender', 'receiver', 'subject', 'body', 'date'], axis=1)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42)
-print(f"Vérification des tailles suite au split:\n- Xtrain: {X_train.shape}\tYtrain {
-      y_train.shape}\n- Xtest: {X_test.shape}\tYtest: {y_test.shape}\n")
 
-# Entrainement du modèle
-classifier = LogisticRegression()
-classifier.fit(X_train, y_train)
-y_pred = classifier.predict(X_test)
+def vectorise(dataframe, vectorizer, name):
+    """Vectorisation du texte"""
+    text_vectorized = vectorizer.fit_transform(dataframe["text"])
+    jb.dump(vectorizer, name)
+    return text_vectorized
 
-# cv_scores = cross_val_score(classifier, X, y, cv=5, scoring="accuracy")
 
-# print(f"Accuracy de chaque fold: {cv_scores}")
-# print(f"Moyenne des taux de classification: {cv_scores.mean()}")
+# Vectorisation des différentes colonnes avec les deux algorithmes (pour les comparer ensuite)
+count_vect = CountVectorizer()
+x_count_vect = vectorise(phish_df, count_vect,
+                         "models/bow_vectorizer.pkl")  # Input du modèle
 
-# Calcul des scores
-accuracy = accuracy_score(y_test, y_pred)
-y_score = classifier.predict_proba(X_test)[:, 1]
-fpr, tpr, _ = roc_curve(y_test, y_score)
-roc_auc = roc_auc_score(y_test, y_score)
+tfidf_vect = TfidfVectorizer()
+x_tfidf_vect = vectorise(phish_df, tfidf_vect,
+                         "models/tfidf_vectorizer.pkl")  # Input du modèle
 
-# Afficher les résultats
-print("\nRésultat du classifieur LogisticRegression:\n")
-print(f"Accuracy: {accuracy}")
-print(f"Précision: {precision_score(y_test, y_pred)}")
-print(f"Recall: {recall_score(y_test, y_pred)}")
-print(f"F1Score: {f1_score(y_test, y_pred)}")
+y = phish_df['isPhishing']  # Output du modèle (à prédire)
 
-ConfusionMatrixDisplay.from_predictions(
-    y_test, y_pred, normalize="true", values_format=".0%")
-plt.title("Matrice de confusion de LogisticRegression")
-plt.show()
 
-plt.figure()
-plt.plot(fpr, tpr, label=f'{"Logistic Regression"} (AUC = {roc_auc:.2f})')
-plt.plot([0, 1], [0, 1], 'k--')
-plt.xlabel('Taux de faux positif')
-plt.ylabel('Taux de vrai positif')
-plt.title(f'Courbe ROC - {"Logistic Regression"}')
-plt.legend(loc='lower right')
-plt.show()
+def train_classifier(x, y):
+    X_train, X_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.3, random_state=42)
 
-# Enregistrement du modèle
-jb.dump(classifier, "models/ml_text_email.pkl")
+    print(f"Vérification des tailles suite au split:\n- Xtrain: {X_train.shape}\tYtrain {
+          y_train.shape}\n- Xtest: {X_test.shape}\tYtest: {y_test.shape}\n")
+
+    classifiers = {
+        "Logistic Regression": LogisticRegression(),
+        "Random Forest": RandomForestClassifier(),
+        "Support Vector Machine": SVC(kernel='linear', probability=True),
+        "Gradient Boosting": GradientBoostingClassifier(),
+    }
+
+    scores = {
+        'Classifier': [],
+        'Accuracy': [],
+        'Precision': [],
+        'Recall': [],
+        'F1-Score': [],
+        'ROC AUC': [],
+    }
+
+    for name, classifier in classifiers.items():
+        classifier.fit(X_train, y_train)
+        y_pred = classifier.predict(X_test)
+
+        # Calcul du score de validation croisée
+        cv_scores = cross_val_score(classifier, x, y, cv=5, scoring="accuracy")
+        print(f"Accuracy de chaque fold: {cv_scores}")
+        print(f"Moyenne des taux de classification: {cv_scores.mean()}")
+
+        # Calcul des scores
+        accuracy = accuracy_score(y_test, y_pred)
+        roc_auc = roc_auc_score(y_test, classifier.predict_proba(X_test)[:, 1])
+
+        # Enregistrer les scores
+        scores['Classifier'].append(name)
+        scores['Accuracy'].append(accuracy)
+        scores['Precision'].append(precision_score(y_test, y_pred))
+        scores['Recall'].append(recall_score(y_test, y_pred))
+        scores['F1-Score'].append(f1_score(y_test, y_pred))
+        scores['ROC AUC'].append(roc_auc)
+
+        # Afficher les résultats
+        print(f"{name}:")
+        print(f"Accuracy: {accuracy}")
+        print(f"ROC AUC Score: {roc_auc:.2f}")
+        ConfusionMatrixDisplay.from_predictions(
+            y_test, y_pred, normalize="true", values_format=".0%")
+        plt.title(f"Matrice de confusion de {name}")
+        plt.show()
+
+        # Convertir en DataFrame
+        scores_df = pd.DataFrame(scores)
+
+    # Graphique pour comparer les métriques
+    plt.figure(figsize=(18, 10))
+
+    scores_melted = scores_df.melt(
+        id_vars='Classifier', var_name='Metric', value_name='Score')
+
+    ax = sns.barplot(x='Classifier', y='Score',
+                     hue='Metric', data=scores_melted)
+
+    for p in ax.patches:
+        height = round(p.get_height(), 2)
+        ax.annotate(f'{height}',
+                    (p.get_x() + p.get_width() / 2., height),
+                    ha='center', va='center',
+                    xytext=(0, 10),
+                    textcoords='offset points')
+
+    plt.title('Comparaison des Scores de Classification')
+    plt.xlabel('Classificateurs')
+    plt.ylabel('Scores')
+    plt.legend(title='Métriques')
+    plt.show()
+
+
+train_classifier(x_count_vect, y)
+train_classifier(x_tfidf_vect, y)
