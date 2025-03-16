0b9b1c1b6d4b109f292da39ad062aea7910a9a09
Oxbian <oxbian@mailbox.org>
2025-03-16 16:06:37 -0400

    feat: hash du contenu pour éviter le recalcul de mail similaire

--- Backend/src/web/routes/mail.rs
+++ Backend/src/web/routes/mail.rs
@@ -8,37 +8,19 @@ use crate::helper::trace::trace_logs;
 pub async fn history(user_data: &USERS) -> String {
     let page = fs::read_to_string("html/mail/history.html").unwrap();
    
-    let messages: Vec<MAIL> = MAIL::get_mails(user_data.user_uuid.clone()).await;
-    let message_html = create_message_list(messages);
+    let mails: Vec<MAIL> = MAIL::get_mails(user_data.user_uuid.clone()).await;
+    let mail_html = create_mail_list(mails);
 
-    return page.replace("{{mails}}", &message_html);
+    return page.replace("{{mails}}", &mail_html);
 }
 
-// Extract matching pattern from string
-fn extract(pattern: &str, mail_content: &String) -> String {
-    let re = match Regex::new(pattern) {
-        Ok(re) => re,
-        Err(_) => return "Not found".to_string(),
-    };
-
-    match re.captures(mail_content.as_str()) {
-        Some(caps) => {
-            match caps.get(0) {
-                Some(data) => data.as_str().to_string(),
-                None => "Not found".to_string()
-            }
-        },
-        None => "Not found".to_string(),
-    }
-}
-
-pub fn create_message_list(messages:Vec<MAIL>) -> String {
+pub fn create_mail_list(mails:Vec<MAIL>) -> String {
   let mut message_html = String::new();
   let base = fs::read_to_string("html/mail/files/mail.html").unwrap();
 
-  for msg in messages {
+  for mail in mails {
 
-    let result: Value = serde_json::from_str(&msg.mail_result).unwrap();
+    let result: Value = serde_json::from_str(&mail.mail_result).unwrap();
     let explication_mail = result["explication_mail"].as_array().unwrap();
 
     // Create string with word: probability
@@ -49,13 +31,10 @@ pub fn create_message_list(messages:Vec<MAIL>) -> String {
         .collect::<Vec<String>>()
         .join(", ");
 
-    trace_logs(msg.mail_content.clone());
-    trace_logs(extract(r"^From: (.*)$", &msg.mail_content));
-
-    let tmp = base.replace("{{mail_id}}", &msg.mail_uuid)
-        .replace("{{mail_sender}}", extract(r"^From: (.*)$", &msg.mail_content).as_str())
-        .replace("{{mail_date}}", extract(r"^Date: (.*)$", &msg.mail_content).as_str())
-        .replace("{{mail_subject}}", extract(r"^Subject: (.*)$", &msg.mail_result).as_str())
+    let tmp = base.replace("{{mail_id}}", &mail.mail_uuid)
+        .replace("{{mail_sender}}", &mail.mail_sender)
+        .replace("{{mail_date}}", &mail.mail_date)
+        .replace("{{mail_subject}}", &mail.mail_subject)
         .replace("{{mail_result}}", result["phishing"].as_str().unwrap_or_else(|| "Not found"))
         .replace("{{mail_reason}}", explication.as_str());
     message_html.push_str(&tmp);
